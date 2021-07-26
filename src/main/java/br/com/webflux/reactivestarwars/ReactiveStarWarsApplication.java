cb17d3b085758bf6852059028df0af3a9ae33a07
ilegra.victor <ilegra.victor@agibank.com.br>
2021-07-26 19:34:46 -0300

    Commit inicial. Cria projeto

---
+++ src/main/java/br/com/webflux/reactivestarwars/ReactiveStarWarsApplication.java
@@ -0,0 +1,13 @@
+package br.com.webflux.reactivestarwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveStarWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveStarWarsApplication.class, args);
+	}
+
+}
