080c04c0194d32d04a9275e6745a00640f430d16
ilegra.victor <ilegra.victor@agibank.com.br>
2021-07-26 19:48:48 -0300

    Cria API de client com a Star Wars API

---
+++ star-wars-api-client/src/main/java/br/com/webflux/starwarsapiclient/StarWarsApiClientApplication.java
@@ -0,0 +1,13 @@
+package br.com.webflux.starwarsapiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsApiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsApiClientApplication.class, args);
+	}
+
+}
